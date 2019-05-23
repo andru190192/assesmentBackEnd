854459ff9986a0204c228b77d4a1578e606b0fab
Danny Tandazo <danny.tb.428@gmail.com>
2019-05-22 19:53:05 -0500

    fix: requestMapping

--- compras-rest/src/main/java/com/compras/backend/rest/controller/ProductController.java
+++ compras-rest/src/main/java/com/compras/backend/rest/controller/ProductController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/product/findAll")
+@RequestMapping("/product/")
 public class ProductController {
 
     @Autowired
