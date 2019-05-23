065661ad998764dc2d451768996c375305ce17e2
Ian <dcaiza@kruger.com.ec>
2019-05-22 19:59:12 -0500

    c

--- src/main/java/com/ian/erp/modules/products/entity/ProductEntity.java
+++ src/main/java/com/ian/erp/modules/products/entity/ProductEntity.java
@@ -20,28 +20,28 @@ import lombok.Setter;
 public class ProductEntity {
 
 	@Id
-	@Column(name = "productcode")
+	@Column(name = "productcode", length = 15)
 	private String productCode;
 
-	@Column(name = "productname", nullable = false)
+	@Column(name = "productname", nullable = false, length = 70)
 	private String productName;
 
-	@Column(name = "productimage", nullable = false)
+	@Column(name = "productimage", nullable = false, length = 50)
 	private String productImage;
 
-	@Column(name = "productline", nullable = false)
+	@Column(name = "productline", nullable = false, length = 50)
 	private String productLine;
 
-	@Column(name = "productscale", nullable = false)
+	@Column(name = "productscale", nullable = false, length = 10)
 	private String productScale;
 
-	@Column(name = "productvendor", nullable = false)
+	@Column(name = "productvendor", nullable = false, length = 50)
 	private String productVendor;
 
 	@Column(name = "productdescription", nullable = false)
 	private String productDescription; // text NOT NULL,
 
-	@Column(name = "quantityinstock", nullable = false)
+	@Column(name = "quantityinstock", nullable = false, length = 6)
 	private String quantitynStock;
 
 	@Column(name = "buyprice", nullable = false)
@@ -50,6 +50,6 @@ public class ProductEntity {
 	@Column(name = "msrp", nullable = false)
 	private Long MSRP;
 
-	@Column(name = "ranking", nullable = false)
+	@Column(name = "ranking", nullable = false, length = 5)
 	private String ranking;
 }
