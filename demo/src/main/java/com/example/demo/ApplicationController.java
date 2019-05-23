61e59ec32e8ce7d6505b980942a40bae27d54841
Paulo Chiliguano <paulo.chiliguano@gmail.com>
2019-05-22 19:39:35 -0500

    Application Controller

---
+++ demo/src/main/java/com/example/demo/ApplicationController.java
@@ -0,0 +1,16 @@
+@RestController
+@RequestMapping(value = "/application")
+public class ApplicationController {
+    @Autowired
+    private ApplicationService applicationService;
+    @RequestMapping(value = "/get-image", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getImagePath() throws IOException {
+        ResponseEntity<byte[]> responseEntity = applicationService.getImageURL();
+        return responseEntity;
+    }
+    @RequestMapping(value = "/get-text", method = RequestMethod.GET)
+    public ResponseEntity<byte[]> getTextPath() throws IOException {
+        ResponseEntity<byte[]> responseEntity = applicationService.getTextData();
+        return responseEntity;
+    }
+}
